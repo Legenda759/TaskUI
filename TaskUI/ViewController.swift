bfd8e99aa9d3717bd30f04d6624af83127ee3117
Legenda759 <oripov1994@gmail.com>
2022-02-01 15:44:32 +0500

    Initial Commit

---
+++ TaskUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskUI
+//
+//  Created by Legenda_759 on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
